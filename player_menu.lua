8c1ce53fc5e7e78400a54905800a3c22f9258ad4
iiMOOV <52023489+iiMOOV@users.noreply.github.com>
2026-08-08 23:58:49 +0300

    Update player_menu.lua

--- player_menu.lua
+++ player_menu.lua
@@ -2340,17 +2340,6 @@ function script.update(dt)
   pcall(function() DriveChat.update(dt) end)
 end
 
--- =================================================================
--- إخفاء وتسكير شات أستو كورسا الأصلي إجباري لجميع اللاعبين
--- =================================================================
-ac.onSessionStart(function ()
-  -- إغلاق نافذة تطبيق الشات الأصلية تلقائياً أول ما يرسبن اللاعب
-  ac.setAppWindowVisible("Chat", false)
-  ac.setAppWindowVisible("chat", false)
-end)
-
--- إخفاء رسائل الشات اللي تطلع فوق في وسط الشاشة (OSD Chat Messages)
-ac.setUIElementsVisibility(ac.UIElement.ChatMessage, false)
 --=================================================================
 -- [25] SCREEN HUD  (الطبقات فوق الشاشة)
 --=================================================================
